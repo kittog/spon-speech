eee859e6abea7c0374ace256407ae947a86c2020
kittog <lena.gaubert@gmail.com>
2023-07-20 15:23:54 +0200

    màj argparse

--- scripts-python/overlaps.py
+++ scripts-python/overlaps.py
@@ -1,47 +1,38 @@
 #!/bin/python3
 # -*- coding: utf-8 -*-
-
 import argparse
-import sys
-import os
-import glob
-
-from praatio import textgrid
 import tgt
+from praatio import textgrid
+from pathlib import Path
 
-def overlap(tiers):
-    for i in range(len(tiers) - 1):
-        over = tgt.util.get_overlapping_intervals(tiers[i], tiers[i+1])
 
+def overlaps(textgrid):
+    tiers = textgrid.tiers
+    for i in range(len(tiers)-1):
         tier = tiers[i]
+        over = tgt.util.get_overlapping_intervals(tiers[i], tiers[i+1])
         for j in range(len(over)):
             start = over[j].start_time
             end = over[j].end_time
-            # del nth overlap
-            tier.delete_annotations_between_timepoints(start, end, left_overlap=True, right_overlap=True)
-    
+            tier.delete_annotations_between_timepoints(
+                start, end, left_overlap=True, right_overlap=True)
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--folder', type=str)
+    parser.add_argument('input', type=str)  # input file
+    parser.add_argument('output', type=str)  # output folder
     args = parser.parse_args()
     return args
 
+
 def main():
-    path_to_folder = parse_args()
-    path_to_files = glob.glob(path_to_folder + "/" + "*.TextGrid")
-    for f in path_to_files:
-        # binary format
-        tg = textgrid.openTextgrid(f, False)
-        tg.save(f, format="long_textgrid", includeBlankSpaces=True)
+    args = parse_args()
+    filename = Path(args.input).stem
+    tg = tgt.io.read_textgrid(args.input)
+    overlaps(tg)
+    tgt.io.write_to_file(tg, f"{args.output}/{filename}")
 
-        tg = tgt.io.read_textgrid(f)
-
-        tiers = tg.tiers
-        overlap(tiers)
-
-        tgt.io.write_to_file(tg, f, format="TextGrid")
-    
 
 if __name__ == '__main__':
     main()
