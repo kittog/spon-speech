1ef8677dee7d24eb78f5a5de8292f19d5882d6ac
kittog <lena.gaubert@gmail.com>
2024-01-31 14:17:24 +0100

    edit: can't be used for diarization

--- scripts/whisper_transcriptions.py
+++ scripts/whisper_transcriptions.py
@@ -1,14 +1,22 @@
+#!/bin/python3
+# -*- coding: utf-8 -*-
+
+# transcription
 import whisper
 import time
+# audio
 import librosa
 import soundfile as sf
+from pydub import AudioSegment
+# texgrid
+from praatio import textgrid
+from praatio.utilities.constants import Interval
+# misc
 import re
 import os
+from pathlib import Path
+# arg parser
 import click
-import tgt
-
-def write_textgrid():
-    pass
 
 @click.command()
 @click.option("--wav", type=str, help="Path to audio file that needs transcriptions.")
@@ -22,8 +30,14 @@ def main():
     model = whisper.load_model("medium")
     results = model.transcribe(wav)
     # write transcriptions to textgrid file
-    tg = tgt.TextGrid()
-
+    tg = textgrid.Textgrid()
+    tier_name = Path(wav).stem
+    entries = []
+    for seg in results['segments']:
+        entries.append((seg['start'], seg['end'], seg['text']))
+    intr = textgrid.IntervalTier(name=tier_name, entries=entries)
+    tg.addTier(intr)
+    tg.save(f"../{tier_name}.TextGrid")
 
 if __name__ == "__main__":
     main()
